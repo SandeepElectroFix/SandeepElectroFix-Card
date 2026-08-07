f35bc08afacda608bf9c9e5e3837de7f6ef741d3
SandeepElectroFix <SandeepElectroFix@gmail.com>
2026-08-07 21:42:39 +0530

    Add feature configuration in config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+/* ==========================================
+   SANDEEP ELECTROFIX CARD
+   FEATURE CONFIG
+========================================== */
+
+const CONFIG = {
+
+  gallery: false,
+
+  reviews: false,
+
+  googleMaps: true,
+
+  youtube: false,
+
+  faq: false,
+
+  offers: false,
+
+  contactForm: true,
+
+  visitorCounter: false,
+
+  darkMode: true
+
+};
